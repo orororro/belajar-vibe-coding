90991404c005d04539d2f30cbe27d34a509516a4
Zidane Ahmad Farabi <zidane@Macbook-Pro.local>
2026-08-21 15:13:43 +0700

    test: add comprehensive integration unit test suite (#14)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { Elysia } from 'elysia';
 import { userRoute } from './routes/user-route';
 
-const app = new Elysia()
+export const app = new Elysia()
   .get('/', () => {
     return { message: 'Hello Elysia' };
   })
-  .use(userRoute)
-  .listen(Number(process.env.PORT) || 3000);
+  .use(userRoute);
 
-console.log(`🦊 Elysia server is running at http://${app.server?.hostname}:${app.server?.port}`);
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(Number(process.env.PORT) || 3000);
+  console.log(
+    `🦊 Elysia server is running at http://${app.server?.hostname}:${app.server?.port}`
+  );
+}
 
 export type App = typeof app;
